47650eb12418ff06f9e9f0cbe7f775762b0b8963
Naveennivas7 <naveennivaskrishna@gmail.com>
2026-06-15 11:28:59 +0530

    added new programs

--- Searchin2Darraay.java
+++ Searchin2Darraay.java
@@ -1,30 +1,42 @@
 import java.util.Arrays;
 
 public class Searchin2Darraay {
-    public static void main(String[]args){
-        int [] [] arr={
-            {1,2,3},
-            {4,5,6},
-            {7,8,9}
+    public static void main(String[] args) {
+        int[][] arr = {
+                { 1, 2, 3 },
+                { 4, 5, 6 },
+                { 7, 8, 9 }
         };
-        int target=5;
-        int[] ans=search(arr,target);  // return in the formate of indec {r}  {c};
+        int target = 5;
+        int[] ans = search(arr, target); // return in the formate of indec {r} {c};
         System.out.println(Arrays.toString(ans));
-        }
-
-
-        static int[] search(int[][]arr,int target){
-
-            for(int r=0;r<arr.length;r++){
-
-                for(int c=0;c<arr[r].length;c++){
-                    if(arr[r][c]==target)
-                        return new int[]{r,c};
-                }
-            }
-            return new int[]{-1,-1};
-        }
-
+        System.out.println(max(arr));
     }
-    
 
+    static int[] search(int[][] arr, int target) {
+
+        for (int r = 0; r < arr.length; r++) {
+
+            for (int c = 0; c < arr[r].length; c++) {
+                if (arr[r][c] == target)
+                    return new int[] { r, c };
+            }
+        }
+        return new int[] { -1, -1 };
+    }
+
+    static int max(int[][] arr) {
+        int max = Integer.MIN_VALUE;
+
+        for (int[] row : arr) {
+
+            for (int element : row) {
+
+                if (element > max)
+                    max = element;
+            }
+        }
+        return max;
+    }
+
+}
