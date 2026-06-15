224f628dc7b6af848f80f95f6234cc479dcca28c
Naveennivas7 <naveennivaskrishna@gmail.com>
2026-06-15 11:04:54 +0530

    added new programs

---
+++ Searchin2Darraay.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+public class Searchin2Darraay {
+    public static void main(String[]args){
+        int [] [] arr={
+            {1,2,3},
+            {4,5,6},
+            {7,8,9}
+        };
+        int target=5;
+        int[] ans=search(arr,target);  // return in the formate of indec {r}  {c};
+        System.out.println(Arrays.toString(ans));
+        }
+
+
+        static int[] search(int[][]arr,int target){
+
+            for(int r=0;r<arr.length;r++){
+
+                for(int c=0;c<arr[r].length;c++){
+                    if(arr[r][c]==target)
+                        return new int[]{r,c};
+                }
+            }
+            return new int[]{-1,-1};
+        }
+
+    }
+    
+
