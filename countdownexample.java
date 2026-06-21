4c19e73257302a0a616fbc89725fe943a4895164
Naveennivas7 <naveennivaskrishna@gmail.com>
2026-06-21 13:36:24 +0530

    comiting mathproblems

---
+++ countdownexample.java
@@ -0,0 +1,11 @@
+public class countdownexample {
+    public static void main(String[]args){
+        int countdown=3;
+        while(countdown>=0){
+            System.out.println(countdown);
+            countdown--;
+        }
+        System.out.println("happy new year");
+    }
+    
+}
---
+++ dowhileexample.java
@@ -0,0 +1,12 @@
+public class dowhileexample {
+    public static void main(String[]args){
+        int i=6;
+
+        do{
+            System.out.println("hi "+ i);
+            i++;
+        }
+        while(i<5);
+    }
+    
+}
