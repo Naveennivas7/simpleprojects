c08a75de142d3cbc59838fac7b1aa43a1443f208
Naveennivas7 <naveennivaskrishna@gmail.com>
2026-06-21 13:57:45 +0530

    comiting mathproblems

---
+++ nestedloopexample.java
@@ -0,0 +1,33 @@
+public class nestedloopexample {
+    public static void main (String [] args){
+        
+    //     for(int i=1;i<=4;i++){
+        
+    //     for(int j =1; j<=4;j++){
+    //         System.out.print("* ");
+    //     }
+    //     System.out.println();
+    // }
+
+
+    //output
+    // * * * *  */
+    // * * * *  */
+    // * * * *  */
+    // * * * *  */
+
+
+
+
+    for(int i=1;i<=6;i++){
+    
+
+    for(int j=1;j<=i;j++){
+        System.out.print(j+" ");
+    }
+    System.out.println();
+    }
+
+    
+}
+}
